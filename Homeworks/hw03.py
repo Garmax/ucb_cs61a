11ff5bb069ebe37b773f614a06e34b5adb51011d
Garmax <shuaiyux@gmail.com>
2019-07-19 15:48:48 +0800

    update

--- Homeworks/hw03.py
+++ Homeworks/hw03.py
@@ -296,19 +296,44 @@ def totals_tree(m):
 # Extra Questions #
 ###################
 
+""" 
+The logician Alonzo Church invented a system of representing non-negative
+integers entirely using functions. The purpose was to show that functions
+are sufficient to describe all of number theory: if we have functions, we
+do not need to assume that numbers exist, but instead we can invent them.
+
+Your goal in this problem is to rediscover this representation known as 
+Church numerals. Here are the definitions of zero, as well as a function 
+that returns one more than its argument: 
+"""
+
 def zero(f):
     return lambda x: x
 
 def successor(n):
     return lambda f: lambda x: f(n(f)(x))
 
+""" 
+First, define functions one and two such that they have the same behavior
+as successor(zero) and successsor(successor(zero)) respectively, but do
+not call successor in your implementation.
+
+Next, implement a function church_to_int that converts a church numeral
+argument to a regular Python integer.
+
+Finally, implement functions add_church, mul_church, and pow_church that
+perform addition, multiplication, and exponentiation on church numerals. 
+"""
+
 def one(f):
     """Church numeral 1: same as successor(zero)"""
     "*** YOUR CODE HERE ***"
+    return lambda x:f(x)
 
 def two(f):
     """Church numeral 2: same as successor(successor(zero))"""
     "*** YOUR CODE HERE ***"
+    return lambda x:f(f(x))
 
 three = successor(two)
 
@@ -325,6 +350,7 @@ def church_to_int(n):
     3
     """
     "*** YOUR CODE HERE ***"
+    return n(lambda x:x+1)(0)
 
 def add_church(m, n):
     """Return the Church numeral for m + n, for Church numerals m and n.
@@ -333,6 +359,7 @@ def add_church(m, n):
     5
     """
     "*** YOUR CODE HERE ***"
+    return lambda f:lambda x:m(f)(n(f)(x))
 
 def mul_church(m, n):
     """Return the Church numeral for m * n, for Church numerals m and n.
@@ -344,6 +371,7 @@ def mul_church(m, n):
     12
     """
     "*** YOUR CODE HERE ***"
+    return lambda f:m(n(f))
 
 def pow_church(m, n):
     """Return the Church numeral m ** n, for Church numerals m and n.
@@ -354,3 +382,17 @@ def pow_church(m, n):
     9
     """
     "*** YOUR CODE HERE ***"
+    return n(m)
+
+""" 
+Church numerals are a way to represent non-negative integers via repeated
+function application. The definitions of zero, one, and two show that each
+numeral is a function that takes a function and repeats it a number of times
+on some argument x.
+
+The church_to_int function reveals how a Church numeral can be mapped to
+our normal notion of non-negative integers using the increment function.
+
+Addition of Church numerals is function composition of the functions of x,
+while multiplication is composition of the functions of f. 
+"""
